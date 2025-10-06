42f1916d6945eb2d3187c7cfd43be597829df5e5
dvd-svg <sanchordesign@gmail.com>
2025-10-06 18:07:38 +0200

    mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
 });
